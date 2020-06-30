9a4e99b87c5cf40219338abca40f7c2a182f12e7
Carol Vasconcelos <cvsouza04@gmail.com>
2020-06-30 17:26:08 -0300

    Finalização do CRUD de Produto

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+  [Route("products")]
+  public class ProductController : ControllerBase
+  {
+    [HttpGet]
+    [Route("")]
+    public async Task<ActionResult<List<Product>>> Get(
+      [FromServices] DataContext context
+    )
+    {
+      // Include faz um join entre tabelas
+      var products = await context
+        .Products
+        .Include(p => p.Category)
+        .AsNoTracking()
+        .ToListAsync();
+
+      return products;
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<ActionResult<Product>> GetById(
+      int id,
+      [FromServices] DataContext context
+    )
+    {
+      var product = await context
+        .Products
+        .Include(p => p.Category)
+        .AsNoTracking()
+        .FirstOrDefaultAsync(p => p.Id == id);
+
+      if (product == null)
+        return NotFound(new { message = "Produto não encontrado." });
+
+      return product;
+    }
+
+    [HttpGet]
+    [Route("categories/{id:int}")]
+    public async Task<ActionResult<List<Product>>> GetByCategory(
+      int id,
+      [FromServices] DataContext context
+    )
+    {
+      var products = await context
+        .Products
+        .Include(p => p.Category)
+        .AsNoTracking()
+        .Where(p => p.CategoryId == id)
+        .ToListAsync();
+
+      return products;
+    }
+
+    [HttpPost]
+    [Route("")]
+    public async Task<ActionResult<Product>> Post(
+      [FromBody] Product model,
+      [FromServices] DataContext context
+    )
+    {
+      if (!ModelState.IsValid)
+        return BadRequest(ModelState);
+
+      try
+      {
+        context.Products.Add(model);
+        await context.SaveChangesAsync();
+
+        // Seta os dados da categoria ao invés de nulo no produto após a persistência
+        var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync(c => c.Id == model.CategoryId);
+        model.Category = category;
+
+        return Ok(model);
+      }
+      catch (Exception)
+      {
+        return BadRequest(new { message = "Não foi possível criar o produto." });
+      }
+    }
+
+    [HttpPut]
+    [Route("{id:int}")]
+    public async Task<ActionResult<Product>> Put(
+      int id,
+      [FromBody] Product model,
+      [FromServices] DataContext context
+    )
+    {
+      if (model.Id != id)
+        return NotFound(new { message = "Produto não encontrado." });
+
+      if (!ModelState.IsValid)
+        return BadRequest(ModelState);
+
+      try
+      {
+        context.Entry<Product>(model).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+
+        return Ok(model);
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        return BadRequest(new { message = "Este registro já foi atualizado." });
+      }
+      catch (Exception)
+      {
+        return BadRequest(new { message = "Não foi possível atualizar o produto." });
+      }
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    public async Task<ActionResult<Product>> Delete(
+      int id,
+      [FromServices] DataContext context
+    )
+    {
+      var product = await context.Products.FirstOrDefaultAsync(p => p.Id == id);
+      if (product == null)
+        return NotFound(new { message = "Produto não encontrado." });
+
+      try
+      {
+        context.Products.Remove(product);
+        await context.SaveChangesAsync();
+
+        return Ok(new { message = "Produto removido com sucesso." });
+      }
+      catch (Exception)
+      {
+        return BadRequest(new { message = "Não foi possível remover o produto." });
+      }
+    }
+  }
+}
