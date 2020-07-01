fc1e958d342aa1e24b2264be3ca4bbc96a61ba0a
Carol Vasconcelos <cvsouza04@gmail.com>
2020-07-01 20:13:28 -0300

    Finalização do CRUD de usuários

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -14,6 +14,18 @@ namespace Shop.Controllers
   [Route("users")]
   public class UserController : Controller
   {
+    [HttpGet]
+    [Route("")]
+    [Authorize(Roles = "manager")]
+    public async Task<ActionResult<List<User>>> Get(
+      [FromServices] DataContext context
+    )
+    {
+      var users = await context.Users.AsNoTracking().ToListAsync();
+
+      return users;
+    }
+
     [HttpPost]
     [Route("")]
     [AllowAnonymous]
@@ -58,6 +70,59 @@ namespace Shop.Controllers
         user = user,
         token = token
       };
+    }
+
+    [HttpPut]
+    [Route("{id:int")]
+    [Authorize(Roles = "manager")]
+    public async Task<ActionResult<User>> Put(
+      int id,
+      [FromBody] User model,
+      [FromServices] DataContext context
+    )
+    {
+      if (!ModelState.IsValid)
+        return BadRequest(ModelState);
+
+      if (id != model.Id)
+        return NotFound(new { message = "Usuário não encontrado." });
+
+      try
+      {
+        context.Entry(model).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+
+        return model;
+      }
+      catch (Exception)
+      {
+        return BadRequest(new { message = "Não foi possível criar o usuário." });
+      }
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    public async Task<ActionResult<User>> Delete(
+      int id,
+      [FromServices] DataContext context
+    )
+    {
+      var user = await context.Users.FirstOrDefaultAsync(u => u.Id == id);
+      if (user == null)
+        return NotFound(new { message = "Usuário não encontrado." });
+
+      try
+      {
+        context.Users.Remove(user);
+        await context.SaveChangesAsync();
+
+        return Ok(new { message = "Usuário removido com sucesso." });
+      }
+      catch (Exception)
+      {
+        return BadRequest(new { message = "Não foi possível remove o usuário." });
+      }
+
     }
   }
 }
